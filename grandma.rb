2581f9b2cb98fd651384f26a12c00a061c286572
Shamel Lakin <christlife4u@gmail.com>
2019-09-14 15:36:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-speak_to_grandma("phrase")
+speak_to_grandma(" ")
   if speak_to_grandma == "Hi Nana, how are you?".downcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?".upcase
